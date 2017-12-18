374940119ee68cdda45dcb0abdd6c0cbc03abd06
axecopfire <braun.sn11@gmail.com>
2017-12-18 13:53:39 -0500

    repos render from ajax req and link to html_url

--- client/components/repo_list.js
+++ client/components/repo_list.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import RepoDetail from './repo_detail';
 
-const Repos = [
+/*const Repos = [
   { title: 'Pthw', url: 'https://github.com/axecopfire/Pthw' },
   { title: 'AndyJ-PCard', url: 'https://github.com/axecopfire/AndyJ-PCard'},
   { title: 'VWC-Projects', url: 'https://github.com/axecopfire/VWC-Projects'}
-];
+];*/
 
-const RepoList = () => {
-  const RenderedRepos = Repos.map(repo => {
-    return <RepoDetail key={repo.title} repo={repo} />
-  });
+const RepoList = (props) => {
+  const RenderedRepos = props.repos.map(repo =>
+    <RepoDetail key={repo.name} repo={repo} />
+  );
 
   return (
     <ul className="media-list list-group">
--- client/main.js
+++ client/main.js
@@ -5,14 +5,21 @@ import axios from 'axios';
 import RepoList from './components/repo_list';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { repos: [] };
+  }
+
   componentWillMount() {
     axios.get('https://api.github.com/users/axecopfire/repos')
-    .then(response => console.log(response));
+    .then(response => this.setState({repos: response.data}));
   }
+
   render() {
     return (
       <div>
-        <RepoList />
+        <RepoList  repos={this.state.repos}/>
       </div>
     );
   }
