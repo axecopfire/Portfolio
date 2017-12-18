044f3850edb45e9daba4b93e825f04e75be573c3
axecopfire <braun.sn11@gmail.com>
2017-12-18 13:20:15 -0500

    axios added

--- client/components/repo_detail.js
+++ client/components/repo_detail.js
@@ -3,7 +3,7 @@ import React from 'react';
 const RepoDetail = (props) => {
     return (
         <li>
-            <a href={props.repo.link}>{props.repo.title}</a>
+            <a href={props.repo.url}>{props.repo.title}</a>
         </li>
     );
 };
--- client/components/repo_list.js
+++ client/components/repo_list.js
@@ -2,18 +2,18 @@ import React from 'react';
 import RepoDetail from './repo_detail';
 
 const Repos = [
-  { title: 'Pthw', link: 'https://github.com/axecopfire/Pthw' },
-  { title: 'AndyJ-PCard', link: 'https://github.com/axecopfire/AndyJ-PCard'},
-  { title: 'VWC-Projects', link: 'https://github.com/axecopfire/VWC-Projects'}
+  { title: 'Pthw', url: 'https://github.com/axecopfire/Pthw' },
+  { title: 'AndyJ-PCard', url: 'https://github.com/axecopfire/AndyJ-PCard'},
+  { title: 'VWC-Projects', url: 'https://github.com/axecopfire/VWC-Projects'}
 ];
 
 const RepoList = () => {
-  const RenderedRepos = Repos.map(function(repo) {
-    return <RepoDetail repo={repo} />
+  const RenderedRepos = Repos.map(repo => {
+    return <RepoDetail key={repo.title} repo={repo} />
   });
 
   return (
-    <ul>
+    <ul className="media-list list-group">
       {RenderedRepos}
     </ul>
   );
