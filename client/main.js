222a3cc3a8497398c59b6f70deef0531a68400a6
axecopfire <braun.sn11@gmail.com>
2017-12-18 13:37:00 -0500

    ajax request mounted and logging

--- client/main.js
+++ client/main.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
+
 import RepoList from './components/repo_list';
 
 class App extends Component {
+  componentWillMount() {
+    axios.get('https://api.github.com/users/axecopfire/repos')
+    .then(response => console.log(response));
+  }
   render() {
     return (
       <div>
@@ -14,4 +20,5 @@ class App extends Component {
 
 Meteor.startup(() => {
   ReactDOM.render(<App />, document.querySelector('.container'));
+
 });
