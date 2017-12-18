bc80802f52d437fd3445facf2c5b274dd820332c
axecopfire <braun.sn11@gmail.com>
2017-12-18 13:55:47 -0500

    del commented out hard coded list

--- client/components/repo_list.js
+++ client/components/repo_list.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import RepoDetail from './repo_detail';
 
-/*const Repos = [
-  { title: 'Pthw', url: 'https://github.com/axecopfire/Pthw' },
-  { title: 'AndyJ-PCard', url: 'https://github.com/axecopfire/AndyJ-PCard'},
-  { title: 'VWC-Projects', url: 'https://github.com/axecopfire/VWC-Projects'}
-];*/
-
 const RepoList = (props) => {
   const RenderedRepos = props.repos.map(repo =>
     <RepoDetail key={repo.name} repo={repo} />
